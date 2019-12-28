48c5afc8bcddaf673e50cb3f2cc17c5975840a99
yourUsername <yourEmail@domain.com>
2019-12-28 16:48:37 -0500

    mau

---
+++ src/main/java/com/example/demo/meth.java
@@ -0,0 +1,26 @@
+/*
+find the average of three elements
+*/
+package com.example.demo;
+
+public class meth {
+
+    public static void main(String[] args) {
+
+        System.out.print( "Find the average" );
+        int fir = 25;
+        int sec = 36;
+        int thr = 66;
+        int sum = 25 + 66 + 36;
+        System.out.print( "the sum is" + sum );
+
+        double avg = sum/3;
+
+      System.out.print( "the average is" + avg);
+
+
+
+        }
+        }
+
+//}
